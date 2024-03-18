eaa4717884e475d3f00980ccad17d608f86a1d8c
Ian Frye <141495647+ianfrye0818@users.noreply.github.com>
2024-03-18 08:28:30 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <HeroSection />
       <AboutMeSection />
       <SkillsSection />
-      <ExperienceSection />
       <WorkSection />
+      <ExperienceSection />
       {/* <TestimonialsSection /> */}
       <ContactSection />
     </>
